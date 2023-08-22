4fc5767146c77a1337ec9d020e46f3345bd9c1b3
manish3203 <manishchavan3203@gmail.com>
2023-08-22 19:16:21 +0530

    Real-Time Example if-else ladder

---
+++ Dart/Statements/if-else/Class_Codes/8_RealTime.dart
@@ -0,0 +1,31 @@
+
+void main() {
+
+  double sgpa = 8.74;
+
+  if(sgpa > 9.50) {
+
+    print("Grade O (Outstanding)");
+  }else if(sgpa > 8.25) {
+
+    print("Grade A+ (Excellent)");
+  }else if(sgpa > 6.75) {
+
+    print("Grade A (Very Good)");
+  }else if(sgpa > 5.75) {
+
+    print("Grade B+ (Good)");
+  }else if(sgpa > 5.25) {
+
+    print("Grade B (Above Average)");
+  }else if(sgpa > 4.75) {
+
+    print("Grade C (Average)");
+  }else if(sgpa > 4.00) {
+
+    print("Grade D (Pass)");
+  }else {
+
+    print("Faile (Go to home)");
+  }
+}
