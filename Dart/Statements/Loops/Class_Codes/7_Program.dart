756117ba1e8d87a367cfd843476c14ca5e3224c3
manish3203 <manishchavan3203@gmail.com>
2023-08-25 12:09:23 +0530

    Loops In Dart

---
+++ Dart/Statements/Loops/Class_Codes/7_Program.dart
@@ -0,0 +1,15 @@
+//Nested for-loop
+
+void main() {
+
+  int number = 9;
+
+  for(int i=1; i<=3; i++) {
+    
+    for(int j=1; j<=3; j++) {
+
+      print(number);
+      number--;
+    }
+  }
+}
---
+++ Dart/Statements/Loops/Class_Codes/9_Program.dart
@@ -0,0 +1,11 @@
+//do while
+
+void main() {
+
+  int i = 10;
+
+  do {
+    
+    print(i);
+  }while(i<10);
+}
