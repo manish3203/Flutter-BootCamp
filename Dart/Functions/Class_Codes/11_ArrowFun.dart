bd4c5e5f7e1117059eaa6b2665c4805a6c283f69
manish3203 <manishchavan3203@gmail.com>
2023-09-11 12:47:32 +0530

    Arrow Function or Lamba Function

---
+++ Dart/Functions/Class_Codes/11_ArrowFun.dart
@@ -0,0 +1,17 @@
+//Arrow Function or Lambda function
+
+int add(int a, int b) => a+b;
+int sub(int a, int b) => a-b;
+int mul(int a, int b) => a*b;
+num div(int a, int b) => a/b;
+
+void main() {
+
+  int x = 10;
+  int y = 20;
+
+  print(add(x, y));
+  print(sub(x, y));
+  print(mul(x, y));
+  print(div(x, y));
+}
