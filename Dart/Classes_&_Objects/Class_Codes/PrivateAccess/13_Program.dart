3a3e675bee363584d0e10eaf076ebc35091a62ac
manish3203 <manishchavan3203@gmail.com>
2023-09-30 19:28:07 +0530

    Private access specifier concept

---
+++ Dart/Classes_&_Objects/Class_Codes/PrivateAccess/13_Program.dart
@@ -0,0 +1,9 @@
+
+import '12_Program.dart';
+
+void main() {
+
+  Player obj = new Player(1,"KLRahul");
+  obj.playerInfo();
+  //obj._jerNo = 18;    Error : The setter '_jerNo' isn't defined for the class 'Player'.
+}
