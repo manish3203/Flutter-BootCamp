5c1278c75812365c35f8b6a77ef334e77f246dd2
manish3203 <manishchavan3203@gmail.com>
2023-11-13 14:02:15 +0530

    Properties List

--- Dart/Collection/Class_Codes/List/12_List.dart
+++ Dart/Collection/Class_Codes/List/12_List.dart
@@ -7,16 +7,23 @@ void main() {
   print(player);
 
   print(player.first);    //virat
+  
   print(player.hashCode); //100
+  
   print(player.isEmpty);  //false
+  
   print(player.isNotEmpty); //true
 
-  print(player.iterator);
+  print(player.iterator);  //Instance of 'ListIterator<dynamic>'
 
   print(player.last);  //shami
+  
   print(player.length); //4
+  
   print(player.reversed); // shami, KLRahul, Rohit, Virat
 
   print(player.runtimeType); //List<dynamic>
+  
   //print(player.single);  //false
+
 }
